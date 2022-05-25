82425821dc253425a45b740b3fc57f3ab17544a7
Abhishekmittalll <abhiiii0207@gmail.com>
2022-05-25 13:47:26 +0530

    added progrsms

---
+++ cpp project/PROG!.cpp
@@ -0,0 +1,31 @@
+// Write a program to find area and perimeter of squarre 
+// same program through function 
+// through 
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+        int s; //declaration of variable
+
+    cout<<"enter a side of a square"<<endl;
+    cin>>s;
+    int area =  s*s;
+    cout<<"Area="<<endl;
+    cout<<area<<endl;
+
+    int p;
+    cout<<"Enter side id square for perimeter "<<endl;
+    cin>>s;
+    p=4*s;
+    cout<<"perimeter",p;
+    return 0;
+    
+
+
+
+        
+    }
+
+
